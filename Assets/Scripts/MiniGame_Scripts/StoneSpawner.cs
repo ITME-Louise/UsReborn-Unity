815f3eb78bba9fe80d0f0703aaf83fd55ae251c4
jineeeAn <jjonghosagwa@gmail.com>
2025-06-01 01:21:49 +0900

    UI 어지러움 -> Camera Layer로 해결/BearGame완료/RabitGameVRTest완료

--- Assets/Scripts/MiniGame_Scripts/StoneSpawner.cs
+++ Assets/Scripts/MiniGame_Scripts/StoneSpawner.cs
@@ -6,8 +6,8 @@ public class StoneSpawner : MonoBehaviour
 {
     // Start is called before the first frame update
     public GameObject stonePrefab;
-    public Transform[] stoneSpawnPoints;
-    public Transform[] stoneSnapTargets;
+    public Transform spawnPoint;             // 1개의 생성 위치
+    public Transform[] stoneSnapTargets;     // 5개의 스냅 목표 위치
 
     void Start()
     {
@@ -16,14 +16,13 @@ public class StoneSpawner : MonoBehaviour
 
     void SpawnStones()
     {
-        for (int i = 0; i < stoneSpawnPoints.Length; i++)
+        for (int i = 0; i < stoneSnapTargets.Length; i++)
         {
-            GameObject stone = Instantiate(stonePrefab, stoneSpawnPoints[i].position, stoneSpawnPoints[i].rotation);
-
+            GameObject stone = Instantiate(stonePrefab, spawnPoint.position, spawnPoint.rotation);
             stone.name = "Stone_" + (i + 1);
 
             StoneSnapper snapper = stone.GetComponent<StoneSnapper>();
-            if (snapper != null && i < stoneSnapTargets.Length)
+            if (snapper != null)
             {
                 snapper.snapTarget = stoneSnapTargets[i];
             }
