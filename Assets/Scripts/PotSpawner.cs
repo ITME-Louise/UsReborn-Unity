95ed4e68673b004f7fc67e94bfd0c63f3df8d13a
alschlee <1daysixmeal@gmail.com>
2025-06-05 03:51:23 +0900

    [FEAT] 화분 프리팹 생성 시 회전값이 초기화되지 않도록 수정 #57

--- Assets/Scripts/PotSpawner.cs
+++ Assets/Scripts/PotSpawner.cs
@@ -42,22 +42,22 @@ public class PotSpawner : MonoBehaviour
 
     private IEnumerator SpawnVaseSequence(Vector3 spawnPosition)
     {
-        GameObject pot1 = Instantiate(flowerVaseStage1, spawnPosition, Quaternion.identity);
+        GameObject pot1 = Instantiate(flowerVaseStage1, spawnPosition, flowerVaseStage1.transform.rotation);
         Debug.Log("1단계 화분 생성");
         yield return new WaitForSeconds(10f);
 
         Destroy(pot1);
-        GameObject pot2 = Instantiate(flowerVaseStage2, spawnPosition, Quaternion.identity);
+        GameObject pot2 = Instantiate(flowerVaseStage2, spawnPosition, flowerVaseStage2.transform.rotation);
         Debug.Log("2단계 화분 생성");
         yield return new WaitForSeconds(10f);
 
         Destroy(pot2);
-        GameObject pot3 = Instantiate(flowerVaseStage3, spawnPosition, Quaternion.identity);
+        GameObject pot3 = Instantiate(flowerVaseStage3, spawnPosition, flowerVaseStage3.transform.rotation);
         Debug.Log("3단계 화분 생성");
         yield return new WaitForSeconds(10f);
 
         Destroy(pot3);
-        Instantiate(flowerVaseStage4, spawnPosition, Quaternion.identity);
+        Instantiate(flowerVaseStage4, spawnPosition, flowerVaseStage4.transform.rotation);
         Debug.Log("최종단계 화분 생성 (3송이)");
     }
 }
