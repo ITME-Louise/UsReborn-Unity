3f9b16a7e9992b67656689dae09b5a39f3694c91
alschlee <1daysixmeal@gmail.com>
2025-06-05 03:19:32 +0900

    [FEAT] 중복 위치에 화분이 겹치지 않도록 거리 기반 생성 제한 기능 추가 #57

--- Assets/Scripts/PotSpawner.cs
+++ Assets/Scripts/PotSpawner.cs
@@ -1,38 +1,61 @@
 using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class PotSpawner : MonoBehaviour
 {
-    public GameObject flowerVaseStage1; // 꽃 없음
-    public GameObject flowerVaseStage2; // 꽃 1송이
-    public GameObject flowerVaseStage3; // 꽃 2송이
-    public GameObject flowerVaseStage4; // 꽃 3송이
+    public GameObject flowerVaseStage1;
+    public GameObject flowerVaseStage2;
+    public GameObject flowerVaseStage3;
+    public GameObject flowerVaseStage4;
+
+    [SerializeField] private float spawnDistanceThreshold = 0.5f;
+    private float spawnDistanceThresholdSqr;
+    private readonly List<Vector3> spawnedPositions = new();
+
+    private void Awake()
+    {
+        spawnDistanceThresholdSqr = spawnDistanceThreshold * spawnDistanceThreshold;
+    }
 
     public void SpawnPot(Vector3 position)
     {
+        if (IsTooCloseToExisting(position))
+        {
+            Debug.Log($"중복 생성 방지됨 (위치: {position})");
+            return;
+        }
+
+        spawnedPositions.Add(position);
         StartCoroutine(SpawnVaseSequence(position));
+    }
+
+    private bool IsTooCloseToExisting(Vector3 newPos)
+    {
+        foreach (var pos in spawnedPositions)
+        {
+            if ((pos - newPos).sqrMagnitude < spawnDistanceThresholdSqr)
+                return true;
+        }
+        return false;
     }
 
     private IEnumerator SpawnVaseSequence(Vector3 spawnPosition)
     {
-        // 1단계
-        var pot1 = Instantiate(flowerVaseStage1, spawnPosition, Quaternion.identity);
+        GameObject pot1 = Instantiate(flowerVaseStage1, spawnPosition, Quaternion.identity);
         Debug.Log("1단계 화분 생성");
         yield return new WaitForSeconds(10f);
 
-        // 2단계
         Destroy(pot1);
-        var pot2 = Instantiate(flowerVaseStage2, spawnPosition, Quaternion.identity);
+        GameObject pot2 = Instantiate(flowerVaseStage2, spawnPosition, Quaternion.identity);
         Debug.Log("2단계 화분 생성");
         yield return new WaitForSeconds(10f);
 
-        // 3단계
         Destroy(pot2);
-        var pot3 = Instantiate(flowerVaseStage3, spawnPosition, Quaternion.identity);
+        GameObject pot3 = Instantiate(flowerVaseStage3, spawnPosition, Quaternion.identity);
         Debug.Log("3단계 화분 생성");
         yield return new WaitForSeconds(10f);
 
-        // 4단계
         Destroy(pot3);
         Instantiate(flowerVaseStage4, spawnPosition, Quaternion.identity);
         Debug.Log("최종단계 화분 생성 (3송이)");
