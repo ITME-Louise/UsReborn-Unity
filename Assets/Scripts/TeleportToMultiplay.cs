22cf548b56dcfcc8b10802713d20163485e8af8c
Kwon Do Hee <doeesoioi3@gmail.com>
2025-06-27 16:13:15 +0900

    Feat: teleport -> 씬 전환 구현

--- Assets/Scripts/TeleportToMultiplay.cs
+++ Assets/Scripts/TeleportToMultiplay.cs
@@ -3,8 +3,12 @@ using Photon.Pun;
 
 public class TeleportToMultiplay : MonoBehaviourPun
 {
-    private void OnTriggerEnter(Collider other)
+    private bool hasTriggered = false; // 중복 방지
+
+    private void OnTriggerStay(Collider other)
     {
+        if (hasTriggered) return;
+
         Debug.Log($"[Teleport Trigger] 충돌 감지됨: {other.name}");
 
         if (other.name.Contains("Index") || other.name.Contains("Hand") || other.CompareTag("PlayerHand"))
@@ -23,13 +27,13 @@ public class TeleportToMultiplay : MonoBehaviourPun
                 case "ToMain":
                     targetScene = "Main_Scene";
                     break;
-                // 필요 시 case 추가 가능
                 default:
                     Debug.LogWarning($"[Teleport] 태그 '{tag}'에 해당하는 씬 없음");
                     return;
             }
 
             Debug.Log($"[Teleport] PhotonNetwork.LoadLevel → {targetScene}");
+            hasTriggered = true;
             PhotonNetwork.LoadLevel(targetScene);
         }
     }
