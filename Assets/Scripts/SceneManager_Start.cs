061e296d9ef492627929342f06ced89b108a2634
jineeeAn <jjonghosagwa@gmail.com>
2025-08-27 00:24:51 +0900

    Controller 상호작용 추가/Start_Scene UI 버튼 클릭 추가/SceneManager->버큰클릭 방식으로 변경

--- Assets/Scripts/SceneManager_Start.cs
+++ Assets/Scripts/SceneManager_Start.cs
@@ -8,10 +8,7 @@ public class SceneManager_Start : MonoBehaviour
     public string sceneToLoad;
     public string subSceneToLoad;
     // Start is called before the first frame update
-    void Start()
-    {
-        Invoke("GoToNextScene", 5f);
-    }
+    
 
     // Update is called once per frame
     public void GoToNextScene()
