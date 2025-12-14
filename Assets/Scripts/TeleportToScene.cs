afadf7b70c821819404cf53bfc7677257527a540
dh2e <doeesoioi3@gmail.com>
2025-12-14 20:59:48 +0900

    [ADD] 텔레포트 포톤 씬마다 배치

--- Assets/Scripts/TeleportToScene.cs
+++ Assets/Scripts/TeleportToScene.cs
@@ -3,13 +3,11 @@ using UnityEngine.SceneManagement;
 
 public class TeleportToScene : MonoBehaviour
 {
-    private bool hasTriggered = false; // 중복 방지용
+    private bool hasTriggered = false;
 
     private void OnTriggerStay(Collider other)
     {
         if (hasTriggered) return;
-
-        //Debug.Log($"[Teleport Trigger] 충돌 감지됨: {other.name}");
 
         if (other.name.Contains("Index") || other.name.Contains("Hand") || other.CompareTag("PlayerHand"))
         {
@@ -18,35 +16,24 @@ public class TeleportToScene : MonoBehaviour
 
             switch (tag)
             {
+                case "ToMulti":
+                    targetScene = "Multi_Scene";
+                    break;
                 case "ToSlam":
                     targetScene = "Slam_Scene";
-                    break;
-                case "ToOasis":
-                    targetScene = "Mini_Oasis";
-                    break;
-                case "ToRabbit":
-                    targetScene = "MiniGame_Rabbit_Scene";
-                    break;
-                case "ToBear":
-                    targetScene = "MiniGame_Bear_Scene";
                     break;
                 case "ToFox":
                     targetScene = "MiniGame_Fox_Scene";
                     break;
-                case "ToPenguin":
-                    targetScene = "MiniGame_Penguin_Scene";
-                    break;
-                case "ToMain":
-                    targetScene = "Main_Scene";
+                case "ToRabbit":
+                    targetScene = "MiniGame_Rabbit_Scene";
                     break;
                 default:
-                    //Debug.LogWarning($"[Teleport] 태그 '{tag}'에 해당하는 씬 없음 (ToMulti 제외됨)");
                     return;
             }
 
-            //Debug.Log($"씬 전환 시도 → {targetScene}");
-            hasTriggered = true; // 중복 호출 방지
+            hasTriggered = true;
             SceneManager.LoadScene(targetScene);
         }
-    } 
+    }
 }
