1c2c69f06b299aa20fe853398559e621f5cbd825
dh2e <doeesoioi3@gmail.com>
2025-12-14 23:57:41 +0900

    [Refactor] teleport to scene 개선

--- Assets/Scripts/TeleportToScene.cs
+++ Assets/Scripts/TeleportToScene.cs
@@ -1,39 +1,39 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using Oculus.Interaction;
 
 public class TeleportToScene : MonoBehaviour
 {
     private bool hasTriggered = false;
 
-    private void OnTriggerStay(Collider other)
+    // Ray Interactable의 When Select 이벤트에서 호출
+    public void OnSelected()
     {
         if (hasTriggered) return;
 
-        if (other.name.Contains("Index") || other.name.Contains("Hand") || other.CompareTag("PlayerHand"))
+        string tag = gameObject.tag;
+        string targetScene = "";
+
+        switch (tag)
         {
-            string tag = gameObject.tag;
-            string targetScene = "";
-
-            switch (tag)
-            {
-                case "ToMulti":
-                    targetScene = "Multi_Scene";
-                    break;
-                case "ToSlam":
-                    targetScene = "Slam_Scene";
-                    break;
-                case "ToFox":
-                    targetScene = "MiniGame_Fox_Scene";
-                    break;
-                case "ToRabbit":
-                    targetScene = "MiniGame_Rabbit_Scene";
-                    break;
-                default:
-                    return;
-            }
-
-            hasTriggered = true;
-            SceneManager.LoadScene(targetScene);
+            case "ToMulti":
+                targetScene = "Multi_Scene";
+                break;
+            case "ToSlam":
+                targetScene = "Slam_Scene";
+                break;
+            case "ToFox":
+                targetScene = "MiniGame_Fox_Scene";
+                break;
+            case "ToRabbit":
+                targetScene = "MiniGame_Rabbit_Scene";
+                break;
+            default:
+                return;
         }
+
+        Debug.Log($"[TeleportToScene] 씬 전환: {targetScene}");
+        hasTriggered = true;
+        SceneManager.LoadScene(targetScene);
     }
 }
